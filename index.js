bd3d5e6427c683c08174f74fe61a1721b791856e
BilalAsif321 <bilalasif3458@gmail.com>
2026-05-29 14:44:51 +0500

    Fix index.js to use REST poller

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const WebSocketClient = require('./src/websocket');
-const AlertEngine = require('./src/alertEngine');
-const EmailService = require('./src/emailService');
-const Store = require('./src/store');
-const logger = require('./src/logger');
+require("dotenv").config();
+const Poller = require("./src/poller");
+const AlertEngine = require("./src/alertEngine");
+const EmailService = require("./src/emailService");
+const Store = require("./src/store");
+const logger = require("./src/logger");
 
 async function main() {
-  logger.info('🚀 PSX Alert System starting...');
-
+  logger.info("PSX Alert System starting - REST polling mode");
   const store = new Store();
   const emailService = new EmailService();
   const alertEngine = new AlertEngine(store, emailService);
-  const wsClient = new WebSocketClient(store, alertEngine);
-
-  // Graceful shutdown
-  process.on('SIGINT', () => {
-    logger.info('Shutting down...');
-    wsClient.disconnect();
-    process.exit(0);
-  });
-
-  process.on('SIGTERM', () => {
-    logger.info('Shutting down...');
-    wsClient.disconnect();
-    process.exit(0);
-  });
-
-  await wsClient.connect();
+  const poller = new Poller(store, alertEngine);
+  process.on("SIGINT", () => { poller.stop(); process.exit(0); });
+  process.on("SIGTERM", () => { poller.stop(); process.exit(0); });
+  await poller.start();
 }
 
-main().catch((err) => {
-  console.error('Fatal error:', err);
-  process.exit(1);
-});
+main().catch((err) => { console.error("Fatal error:", err); process.exit(1); });
